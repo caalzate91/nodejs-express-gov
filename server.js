0ece028115a9bcbd0c35765e9ae0e24e6cf53809
caalzate91 <caalzate91@gmail.com>
2021-02-11 20:58:47 -0500

    Update server.js
    CAmbios en el root del aplicativo

--- server.js
+++ server.js
@@ -142,5 +142,5 @@ app.delete("/api/opportunity/:id", (req, res, next) => {
 
 // Root path
 app.get("/", (req, res, next) => {
-    res.json({"message":"Ok Google.!"})
+    res.json({"message":"Hola a todos en GDG Open Lima.!"})
 })
